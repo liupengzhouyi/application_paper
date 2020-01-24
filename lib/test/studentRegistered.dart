4ce924ab6c7394a5764b0271db3389396ee4ad52
liupeng <liupeng.0@outlook.com>
2020-01-24 23:37:29 +0800

    学生详细信息注册 V 0.0.11

--- lib/test/studentRegistered.dart
+++ lib/test/studentRegistered.dart
@@ -1,63 +1,111 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MyApp());
-}
 
+void main() => runApp(Lll());
 
-class MyApp extends StatelessWidget{
+class Lll extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-              "flutter Demo",
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Landing(),
+    );
+  }
+
+}
+
+
+class Landing extends StatefulWidget {
+
+  Landing({Key key}):super(key:key);
+
+  @override
+  _LandingState createState() => _LandingState();
+
+}
+
+class _LandingState extends State<Landing> {
+
+  //定义一个controller
+  TextEditingController _nameController = TextEditingController();
+
+  //定义一个controller
+  TextEditingController _phoneNumberController = TextEditingController();
+
+  //定义一个controller
+  TextEditingController _studentIDController = TextEditingController();
+
+  String _name = "";
+  String _phonenumber = "";
+  String _studentid = "";
+
+
+  void _incrementCounter() {
+    setState(() {
+      this._name = this._nameController.text;
+      this._phonenumber = this._phoneNumberController.text;
+      this._studentid = this._studentIDController.text;
+      print(this._name + " - " + this._studentid + "-" + this._phonenumber);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:Container(
+          width: 250.00,
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: TextField(
+                  autofocus: true,
+                  controller: this._studentIDController,
+                  decoration: InputDecoration(
+                    labelText: "学号",
+                    hintText: "学号",
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+              ),
+              Container(
+                width: 250.00,
+                child: TextField(
+                  controller: this._nameController,
+                  decoration: InputDecoration(
+                    labelText: "姓名",
+                    hintText: "实名制",
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                ),
+              ),
+              Container(
+                width: 250.00,
+                child: TextField(
+                  controller: this._phoneNumberController,
+                  decoration: InputDecoration(
+                    labelText: "联系方式",
+                    hintText: "您的可接收短信息的联系方式",
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        body: Center(
-          child: Container(
-            child: Text(
-                "xxxxdecoration: BoxDecoratid"
-                    "ecoration: BoxDecoratidecoration"
-                    ": BoxDecoratideco"
-                    "decoration: BoxDecorati"
-                    "ration: BoxDecoratidecoration: BoxDecorati",
-                textAlign: TextAlign.left,
-                // 超出设置
-                overflow: TextOverflow.fade,
-                // 最大显示行数
-                maxLines: 10,
-                textScaleFactor: 1,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w200,
-                  fontStyle: FontStyle.italic,
-                  decoration: TextDecoration.lineThrough,
-                  decorationColor: Colors.black87,
-                  decorationStyle: TextDecorationStyle.solid,
-                ),
-            ),
-            height: 300.00,
-            width: 300.00,
-            decoration: BoxDecoration(
-                color: Colors.amberAccent,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2.00,
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20),
-                ),
-            ),
-            // 内容到边框的距离
-            //padding: EdgeInsets.all(20),
-            padding: EdgeInsets.fromLTRB(10, 30, 10, 20)
-          ),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.file_upload),
+      ),
+      appBar: AppBar(
+        title: Text("学生详细信息注册"),
       ),
     );
   }
