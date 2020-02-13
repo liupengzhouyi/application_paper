f5d39633a40748d07d355396e3e6dad178953848
liupeng <liupeng.0@outlook.com>
2020-02-13 23:36:01 +0800

    教师公告.初始化 V 0.4.12

---
+++ lib/model/TacherAD/ADCrad.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class TeacherADCard extends StatefulWidget {
+  @override
+  _TeacherADCardState createState() => _TeacherADCardState();
+}
+
+class _TeacherADCardState extends State<TeacherADCard> {
+
+  var returnObject = {
+    'id': 1,
+    'authorid': "6969001",
+    'datetime': "2020-01-16T00:00:00.000+0000",
+    'title': "第一次系统公告",
+    'content': "欢迎大家来带这个垃圾学校，选了一个垃圾专业。"
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
